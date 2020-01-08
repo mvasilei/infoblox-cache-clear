fcd35f0eef8feb20eb998e098331314e416c3557
mvasilei <53435274+mvasilei@users.noreply.github.com>
2020-01-08 08:12:47 +0000

    Create infoblox-cache-clear.py

---
+++ infoblox-cache-clear.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+import requests,json
+
+def clear_cache(list):
+	for host in list:
+		params = (('host_name', host),)
+
+		response = requests.get('https://infoblox/wapi/v2.9.1/member:dns', params=params, verify=False, auth=('user', 'pass'))
+
+		if response.status_code == requests.codes.ok:
+			r = response.json()
+
+			for dict in r:
+				params = (('_function', 'clear_dns_cache'),)
+				response = requests.post('https://infoblox/wapi/v2.9.1/' + dict['_ref'], params=params, verify=False, auth=('user', 'pass'))
+
+				if response.status_code == requests.codes.ok:
+					cache_response = response.json()
+
+if __name__ == '__main__':
+	host_list = ['ukx1dn65.msm.','ukx1dn64.msm']
+	clear_cache(host_list)
