e6eb5027a82e4001c680aa844e5c10e18994944f
mvasilei <53435274+mvasilei@users.noreply.github.com>
2020-01-08 08:13:20 +0000

    Update infoblox-cache-clear.py

--- infoblox-cache-clear.py
+++ infoblox-cache-clear.py
@@ -18,5 +18,5 @@ def clear_cache(list):
 					cache_response = response.json()
 
 if __name__ == '__main__':
-	host_list = ['ukx1dn65.msm.','ukx1dn64.msm']
+	host_list = ['ukx1dn65.msm','ukx1dn64.msm']
 	clear_cache(host_list)
